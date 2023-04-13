741b3d39bae876fa5fd981b2af391ffdd4243a5c
Kvssa250 <122424551+Kvssa250@users.noreply.github.com>
2023-04-12 21:16:48 -0400

    Add files via upload

---
+++ Examen/Conductor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Conductor
+    {
+        public string Nombre_completo { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Licencia { get; set; }
+        public string Fecha_de_nacimiento { get; set; }
+    }
+}
---
+++ Examen/Vehiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Vehiculo
+    {
+        public string Placa { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public string Numero_de_chasis { get; set; }
+        public string Year { get; set; }
+    }
+}
